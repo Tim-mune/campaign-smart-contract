e6340177eaef3d5e103bb1a411b75949f2caf720
Timmy <neshtimo38@gmail.com>
2023-08-20 20:57:52 +0300

    modified updated on solc and web3.js

--- ethereum/compile.js
+++ ethereum/compile.js
@@ -6,17 +6,36 @@ const buildPath = path.resolve(__dirname, "build");
 fs.removeSync(buildPath);
 const campaignPath = path.resolve(__dirname, "contracts", "Campaign.sol");
 const source = fs.readFileSync(campaignPath, "utf-8");
+// config file for solc
+const input = {
+  language: 'Solidity',
+  sources: {
+    'campaign.sol': {
+      content: source,
+    },
+  },
+  settings: {
+    outputSelection: {
+      '*': {
+        '*': ['*'],
+      },
+    },
+  },
+};
+
 
 // compile
-const output = solc.compile(source, 1).contracts;
+const output = solc.compile(JSON.stringify(input)).contracts
 console.log(output);
+
 
 // create that build folder
 fs.ensureDirSync(buildPath);
 
 for (let contract in output) {
-  fs.outputJsonSync(
-    path.resolve(buildPath, contract.replace(":", "") + ".json"),
-    output[contract]
-  );
+  // console.log(contract)
+  // fs.outputJsonSync(
+  //   path.resolve(buildPath, contract.replace(":", "") + ".json"),
+  //   output[contract]
+  // );
 }
